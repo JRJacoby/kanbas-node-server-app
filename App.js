4196e8816a10698e55291f9f4b91302da2ef2c9b
John <jrdjacoby@gmail.com>
2024-03-31 15:54:04 -0400

    finished assignments

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js"
 import CourseRoutes from './Kanbas/Database/courses/routes.js';
 import ModuleRoutes from './Kanbas/Database/modules/routes.js';
+import AssignmentRoutes from './Kanbas/Database/assignments/routes.js';
 import cors from "cors"
 
 const app = express()
 app.use(cors())
 app.use(express.json());
 
+AssignmentRoutes(app)
 ModuleRoutes(app)
 CourseRoutes(app);
 Lab5(app);
--- Kanbas/Database/modules/routes.js
+++ Kanbas/Database/modules/routes.js
@@ -26,7 +26,7 @@ function ModuleRoutes(app) {
 	app.get("/api/courses/:cid/modules", (req, res) => {
 		const {cid} = req.params
 		const modules = db.modules.filter((m) => m.course === cid)
-	res.send(modules)
+		res.send(modules)
 	})
 }
 
