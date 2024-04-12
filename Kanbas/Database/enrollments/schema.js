f9345d1d1a024adda14a4a5b10db41e5e8c1961c
John <jrdjacoby@gmail.com>
2024-04-12 17:07:59 -0400

    submittable functionality. still need to do CSS for profile and signin as well as figure out remote deployment of DB

--- Kanbas/Database/enrollments/schema.js
+++ Kanbas/Database/enrollments/schema.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
 
 const enrollmentSchema = new mongoose.Schema({
-	user: {type: String, required: true},
-	course: {type: String, required: true}
+	user: {type: mongoose.Schema.Types.ObjectId, ref: 'UserModel', required: true},
+	course: {type: mongoose.Schema.Types.ObjectId, ref: 'CourseModel', required: true}
 }, {collection: "enrollments"})
 
 export default enrollmentSchema
--- Kanbas/Database/modules/dao.js
+++ Kanbas/Database/modules/dao.js
@@ -1,6 +1,6 @@
 import model from "./model.js"
 
-export const updateModule = (mid, module) => model.updateOne({_id: mid}, { $set: module })
+export const updateModule = (mid, module) => model.findOneAndUpdate({_id: mid}, {$set: module}, {new: true})
 export const deleteModule = (mid) => model.deleteOne({_id: mid})
 export const createModule = (module) => {delete module._id; return model.create(module)}
 export const findModulesForCourse = (cid) => model.find({course: cid})
